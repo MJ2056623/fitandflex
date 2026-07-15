432bd135bbb2506d676f906e52aba22a34e94ab0
Vintagemj <mark_bernal@dmc.edu.ph>
2026-07-15 17:30:23 +0800

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5185/api"
+    baseURL: "https://fitandflex.runasp.net/api"
 });
 
 api.interceptors.request.use((config) => {
